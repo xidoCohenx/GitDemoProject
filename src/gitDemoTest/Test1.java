d8fede42839919a1803cafee9017b4f390cf9ec0
idoCohen <ido cohen@10.0.0.11>
2020-05-30 17:13:43 +0300

    First commit...

---
+++ src/gitDemoTest/Test1.java
@@ -0,0 +1,15 @@
+package gitDemoTest;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungry = "yes";
+		
+		if(hungry == "yes") {
+			System.out.println("eat somthing");
+		}
+		else {
+			System.out.println("do work");
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module gitDemoProject {
+}
