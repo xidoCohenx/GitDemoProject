71b024ba073e755be74fa87d3cafe72980ac85e7
idoCohen <ido cohen@10.0.0.11>
2020-06-01 19:40:23 +0300

    trying to push commit to branch 2

--- src/gitDemoTest/Test1.java
+++ src/gitDemoTest/Test1.java
@@ -1,20 +1,22 @@
 package gitDemoTest;
 
 public class Test1 {
-	public static void main(String[] args) {}
-	
+	public static void main(String[] args) {
+	}
+
 	public void gitRepPusher() {
 		System.out.println("team > commit > name the commit > commit + push > ");
-				
+
 	}
-	
+
 	public void orKingifier() {
 		System.out.println(" i hereby make you a king ");
 	}
-	
+
 	public void branchMaker() {
-		System.out.println("team > commit > name the commit > commit + push > configure > re-select the branch  ");
-		
+		System.out.println("team > commit > name the commit > commit + push > configure > add >"
+				+ "the eclipse should automaticly recognize the branches that exists in the github repository  ");
+
 	}
 
 }
